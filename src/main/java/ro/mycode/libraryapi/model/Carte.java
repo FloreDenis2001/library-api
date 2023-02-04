3a501b4f88ca2ed325cb80df517efac8f04d4796
FloreDenis2001 <floredenis907@yahoo.com>
2023-02-04 18:26:42 +0200

    create_model_done

--- src/main/java/ro/mycode/libraryapi/model/Carte.java
+++ src/main/java/ro/mycode/libraryapi/model/Carte.java
@@ -4,21 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 import java.time.LocalDate;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
-@Table(name="carte")
-@Entity(name="Carte")
+@Table(name = "carte")
+@Entity(name = "Carte")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Carte {
+public class Carte implements Comparable<Carte> {
 
     @Id
-    @SequenceGenerator(name="book_sequence",
+    @SequenceGenerator(name = "book_sequence",
             sequenceName = "book_sequence",
             allocationSize = 1
     )
@@ -28,30 +30,69 @@ public class Carte {
             generator = "book_sequence"
     )
 
-    @Column(name="id")
+    @Column(name = "id")
 
-    private long id;
+    private Long id;
 
 
-   @Column (name="book_name",
-   nullable = false,columnDefinition = "TEXT")
+    @Column(name = "book_name",
+            nullable = false, columnDefinition = "TEXT")
 
     @NotEmpty
     private String book_name;
 
-   @Column(
-           name="create_at",
-           nullable=false,
-           columnDefinition = "DATE"
-   )
+    @Column(
+            name = "create_at",
+            nullable = false,
+            columnDefinition = "DATE"
+    )
 
     private LocalDate created_at;
 
-   @Column(
-           name="description",
-           nullable = false,
-           columnDefinition = "TEXT"
-   )
-   @Not
+    @Column(
+            name = "description",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NotEmpty
     private String description;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "student_id_fk")
+    )
+    private Student student;
+
+
+    @Override
+    public String toString() {
+        String text = "Id : " + this.id;
+        text += "Book Name : " + this.book_name;
+        text += "Description : " + this.description;
+        text += "Student UserName: " + this.student.getUserName();
+        text += "Create At : " + this.created_at;
+
+        return text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Carte x = (Carte) o;
+        return x.getStudent() == this.student;
+    }
+
+    @Override
+    public int compareTo(Carte c) {
+
+        if (c.getCreated_at().getYear() > this.getCreated_at().getYear()) {
+            return 1;
+        } else if (c.getCreated_at().getYear() < this.getCreated_at().getYear()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
 }
--- src/main/java/ro/mycode/libraryapi/model/Student.java
+++ src/main/java/ro/mycode/libraryapi/model/Student.java
@@ -1,4 +1,124 @@
 package ro.mycode.libraryapi.model;
 
-public class Student {
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Table(name="student")
+@Entity(name="Student")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student implements Comparable<Student>{
+   @Id
+   @SequenceGenerator(
+           name="student_sequence",
+           sequenceName = "student_sequence",
+           allocationSize = 1
+   )
+
+   @GeneratedValue(
+           strategy = SEQUENCE,
+          generator = "books_sequence"
+   )
+
+   @Column(
+           name="id"
+   )
+   private Long id;
+
+
+   @Column(
+           name="student_username",
+           nullable = false,
+           columnDefinition = "TEXT"
+   )
+   @NotEmpty
+  private String userName;
+   @Column(
+           name="student_password",
+           nullable = false,
+           columnDefinition = "TEXT"
+   )
+   @NotEmpty
+  private String password;
+
+   @Column(
+           name="student_email",
+           nullable = false,
+           columnDefinition = "TEXT"
+   )
+   @NotEmpty
+  private String email;
+
+    @Column(
+            name="student_email",
+            nullable = false,
+            columnDefinition = "INTEGER"
+    )
+    @NotEmpty
+    private int varsta;
+
+    @Column(
+            name="student_phone",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NotEmpty
+    private String phone;
+
+
+    @OneToMany(mappedBy = "student",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Carte> bookList = new ArrayList<>();
+
+    @Override
+    public String toString(){
+        String text=" Id : "+this.id;
+        text+="UserName : "+this.userName;
+        text+="Password : "+this.password;
+        text+="Email : "+this.email;
+        text+="Phone : "+this.phone;
+
+        return text;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        Student s= (Student) o;
+        return s.password.compareTo(this.getPassword())==0;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(o.getVarsta()>this.getVarsta()){
+            return 1;
+        }else if(o.getVarsta()<this.getVarsta()){
+            return -1;
+        }else {
+            return 0;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 }
